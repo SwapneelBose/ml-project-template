9982c6363480ad3859204503b57643d1f936ae02
MAPLE0934 <swapneelbose70@gmail.com>
2025-10-28 17:02:15 +0530

    Formatted code with black and isort

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+
 from src.model import predict
 
 app = FastAPI(title="ML Project Template")
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World! FastAPI is running 🚀"}
+
 
 @app.get("/predict")
 def get_prediction(x: float):
